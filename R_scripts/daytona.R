da98b33220a8d89e4519ed91f7c3865f460cfaf0
Minh Tang <mtang8@ncsu.edu>
2021-03-17 20:38:33 -0400

    Rename things

---
+++ R_scripts/daytona.R
@@ -0,0 +1,151 @@
+daytona_plot <- function(){
+library(tidyverse)
+
+DAY <- read_table("http://academic.udayton.edu/kissock/http/Weather/gsod95-current/OHDAYTON.txt", col_names = FALSE)
+names(DAY) <- c("Month", "Day", "Year", "Temp")
+DAY <- select(DAY, "Year", "Month", "Day", "Temp")
+# create dataframe that represents 1995-2013 historical data
+Past <- DAY %>%
+  group_by(Year) %>%
+  mutate(newDay = seq(1, length(Day))) %>%          
+  ungroup() %>%
+  filter(Temp != -99 & Year != 2014) %>%     
+  group_by(newDay) %>%
+  mutate(upper = max(Temp), # identify max value for each day
+         lower = min(Temp), # identify min value for each day
+         avg = mean(Temp),  # calculate mean value for each day
+         se = sd(Temp)/sqrt(length(Temp))) %>%  
+  mutate(avg_upper = avg+(2.101*se),  
+         avg_lower = avg-(2.101*se)) %>%  
+  ungroup()
+
+# create dataframe that represents current year data
+Present <- DAY %>%
+  group_by(Year, Month) %>%
+  arrange(Day, .by_group = TRUE) %>%
+  ungroup() %>%
+  group_by(Year) %>%
+  mutate(newDay = seq(1, length(Day))) %>%  
+  ungroup() %>%
+  filter(Temp != -99 & Year == 2014)  
+
+# create dataframe that represents the lowest temp for each day for the historical data
+PastLows <- Past %>%
+  group_by(newDay) %>%
+  summarise(Pastlow = min(Temp)) # identify lowest temp for each day from 1995-2013
+
+# create dataframe that identifies the days in 2014 in which the temps were lower than all previous 19 years
+PresentLows <- Present %>%
+  left_join(PastLows) %>%  
+  mutate(record = ifelse(Temp<Pastlow, "Y", "N")) %>% 
+  filter(record == "Y")  
+
+# create dataframe that represents the highest temp for each day for the historical data
+PastHighs <- Past %>%
+  group_by(newDay) %>%
+  summarise(Pasthigh = max(Temp))  
+
+# create dataframe that identifies the days in 2014 in which the temps were higher than all previous 19 years
+PresentHighs <- Present %>%
+  left_join(PastHighs) %>%  
+  mutate(record = ifelse(Temp>Pasthigh, "Y", "N")) %>% 
+  filter(record == "Y") 
+
+dgr_fmt <- function(x, ...) {
+  parse(text = paste(x, "*degree", sep = ""))
+}
+
+# create y-axis variable
+a <- dgr_fmt(seq(-20,100, by=10))
+
+# create a small dataframe to represent legend symbol for 2014 Temperature
+legend_data <- data.frame(x=seq(178,185),y=rnorm(8,15,2))
+
+p <- ggplot(Past, aes(newDay, Temp)) + 
+  theme(plot.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank()) +
+  geom_linerange(Past, mapping=aes(x=newDay, ymin=lower, ymax=upper), colour = "wheat2", alpha=.1)
+
+p <- p + geom_linerange(Past, mapping=aes(x=newDay, ymin=avg_lower, ymax=avg_upper), colour = "wheat4")
+
+p <- p +  geom_line(Present, mapping=aes(x=newDay, y=Temp, group=1)) +
+  geom_vline(xintercept = 0, colour = "wheat4", linetype=1, size=1)
+
+# p <- p + geom_hline(yintercept = -20, colour = "white", linetype=1) +
+#     geom_hline(yintercept = -10, colour = "white", linetype=1) +
+#     geom_hline(yintercept = 0, colour = "white", linetype=1) +
+#     geom_hline(yintercept = 10, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 20, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 30, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 40, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 50, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 60, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 70, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 80, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 90, colour = "white", linetype=1) +
+#         geom_hline(yintercept = 100, colour = "white", linetype=1)
+
+p <- p + geom_vline(xintercept = 31, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 59, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 90, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 120, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 151, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 181, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 212, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 243, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 273, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 304, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 334, colour = "wheat4", linetype=3, size=.5) +
+  geom_vline(xintercept = 365, colour = "wheat4", linetype=3, size=.5) 
+
+p <- p + coord_cartesian(ylim = c(-20,100)) +
+  scale_y_continuous(breaks = seq(-20,100, by=10), labels = a) +
+  scale_x_continuous(expand = c(0, 0),                       breaks=c(15,45,75,105,135,165,195,228,258,288,320,350),
+                     labels = c("Jan", "Feb", "Mar", "Apr",
+                                "May", "June", "July", "Aug", "Sep",
+                                "Oct", "Nov", "Dec"))
+
+p <- p +
+  geom_point(data=PresentLows, aes(x=newDay, y=Temp), colour="blue3") +
+  geom_point(data=PresentHighs, aes(x=newDay, y=Temp), colour="firebrick3")
+
+p <- p +
+  ggtitle("Dayton's Weather in 2014") +
+  theme(plot.title=element_text(face="bold",hjust=.012,vjust=.8,colour="#3C3C3C",size=20)) +
+  annotate("text", x = 119, y = 98, label = "Temperature", size=4, fontface="bold")
+
+p <- p +
+  annotate("text", x = 126, y = 93, 
+           label = "Data represents average daily temperatures. Accessible data dates back to", size=3, colour="gray30") +
+  annotate("text", x = 122, y = 89, 
+           label = "January 1, 1995. Data for 2014 is only available through December 16.", size=3, colour="gray30") +
+  annotate("text", x = 124, y = 85, 
+           label = "Average temperature for the year was 51.9° making 2014 the 9th coldest", size=3, colour="gray30") +
+  annotate("text", x = 128, y = 81, label = "year since 1995", size=3, colour="gray30")
+
+p <- p +
+  annotate("segment", x = 30, xend = 40, y = -5, yend = -10, colour = "blue3") +
+  annotate("text", x = 65, y = -10, label = "We had 35 days that were the", size=3, colour="blue3") +
+  annotate("text", x = 56, y = -14, label = "coldest since 1995", size=3, colour="blue3") +
+  annotate("segment", x = 302, xend = 307, y = 74, yend = 82, colour = "firebrick3") +
+  annotate("text", x = 333, y = 92, label = "We had 19 days that were the", size=3, colour="firebrick3") +
+  annotate("text", x = 324, y = 88, label = "hottest since 1995", size=3, colour="firebrick3")
+
+p <- p +
+  annotate("segment", x = 181, xend = 181, y = 5, yend = 25, colour = "wheat2", size=3) +
+  annotate("segment", x = 181, xend = 181, y = 12, yend = 18, colour = "wheat4", size=3) +
+  geom_line(data=legend_data, aes(x=x,y=y)) +
+  annotate("segment", x = 183, xend = 185, y = 17.7, yend = 17.7, colour = "wheat4", size=.5) +
+  annotate("segment", x = 183, xend = 185, y = 12.2, yend = 12.2, colour = "wheat4", size=.5) +
+  annotate("segment", x = 185, xend = 185, y = 12.2, yend = 17.7, colour = "wheat4", size=.5) +
+  annotate("text", x = 205, y = 14.75, label = "NORMAL RANGE", size=2, colour="gray30") +
+  annotate("text", x = 152, y = 14.75, label = "2014 TEMPERATURE", size=2, colour="gray30") +
+  annotate("text", x = 205, y = 25, label = "RECORD HIGH", size=2, colour="gray30") +
+  annotate("text", x = 205, y = 5, label = "RECORD LOW", size=2, colour="gray30")
+return(p)
+}
---
+++ R_scripts/hotdogs.R
@@ -0,0 +1,89 @@
+hotdogs_plot <- function(){
+library(mdsr)
+hd <- readr::read_csv(
+  "http://datasets.flowingdata.com/hot-dog-contest-winners.csv")
+names(hd) <- gsub(" ", "_", names(hd)) %>% tolower()
+
+new_data <- data.frame(
+  year = c(1979, 1978, 1974, 1972, 1916), 
+  winner = c(NA, "Walter Paul", NA, NA, "James Mullen"),
+  dogs_eaten = c(19.5, 17, 10, 14, 13),
+  country = rep(NA, 5), new_record = c(1,1,0,0,0)
+)
+hd <- bind_rows(hd, new_data)
+
+xlabs <- c(1916, 1972, 1980, 1990, 2007)
+ylabs <- seq(from = 0, to = 70, by = 10)
+
+hd_plot <- hd %>% filter(year < 2008)
+p <- ggplot(data = hd_plot, aes(x = year, y = dogs_eaten)) +
+  geom_bar(stat = "identity")
+p
+ticks_y <- data.frame(x = 1912, y = ylabs)
+
+text <- bind_rows(
+  # Frank Dellarosa
+  data.frame(x = 1951.5, y = 37, 
+    label = paste("Frank Dellarosa eats 21 and a half HDBs over 12\n",
+      "minutes, breaking the previous record of 19 and a half."), adj = 0),
+  # Joey Chestnut
+  data.frame(x = 1976.5, y = 69, 
+    label = paste("For the first time since 1999, an American\n",
+      "reclaims the title when Joey Chestnut\n",
+      "consumes 66 HDBs, a new world record."), adj = 0), 
+  # Kobayashi
+  data.frame(x = 1960.5, y = 55, 
+    label = paste("Through 2001-2005, Takeru Kobayashi wins by no less\n",
+      "than 12 HDBs. In 2006, he only wins by 1.75. After win-\n",
+      "ning 6 years in a row and setting the world record 4 times,\n",
+      "Kobayashi places second in 2007."), adj = 0),
+  # Walter Paul
+  data.frame(x = 1938, y = 26, label = "Walter Paul sets a new
+world record with 17 HDBs.", adj = 0),
+  # James Mullen
+  data.frame(x = 1917, y = 10, label = "James Mullen wins the inaugural
+contest, scarfing 13 HDBs. Length
+of contest unavailable.", adj = 0), 
+  data.frame(x = 1935, y = 72, label = "NEW WORLD RECORD"),
+  data.frame(x = 1914, y = 72, label = "Hot dogs and buns (HDBs)"),
+#  data.frame(x = 1940, y = 2, 
+#    label = "*Data between 1916 and 1972 were unavailable"), 
+#  data.frame(x = 1922, y = 2, label = "Source: FlowingData")
+)
+
+segments <- bind_rows(
+  data.frame(x = c(1984, 1991, 1991, NA), y = c(37, 37, 21, NA)),
+  data.frame(x = c(2001, 2007, 2007, NA), y = c(69, 69, 66, NA)), 
+  data.frame(x = c(1995, 2006, 2006, NA), y = c(58, 58, 53.75, NA)),
+  data.frame(x = c(2005, 2005, NA), y = c(58, 49, NA)),
+  data.frame(x = c(2004, 2004, NA), y = c(58, 53.5, NA)),
+  data.frame(x = c(2003, 2003, NA), y = c(58, 44.5, NA)),
+  data.frame(x = c(2002, 2002, NA), y = c(58, 50.5, NA)),
+  data.frame(x = c(2001, 2001, NA), y = c(58, 50, NA)),
+  data.frame(x = c(1955, 1978, 1978), y = c(26, 26, 17)))
+
+p <- p + geom_bar(stat = "identity", aes(fill = factor(new_record))) +
+  geom_hline(yintercept = 0, color = "darkgray") + 
+  scale_fill_manual(name = NULL, 
+    values = c("0" = "#006f3c", "1" = "#81c450")) +
+  scale_x_continuous(name = NULL, breaks = xlabs, minor_breaks = NULL, 
+    limits = c(1912, 2008), expand = c(0, 1)) +
+  scale_y_continuous(name = NULL, breaks = ylabs, labels = NULL, 
+    minor_breaks = NULL, expand = c(0.01, 1)) + 
+  geom_text(data = ticks_y, aes(x = x, y = y + 2, label = y), size = 3) +
+  ggtitle("Winners from Nathan's hot dog eating contest") +
+  geom_text(data = text, aes(x = x, y = y, label = label), 
+    hjust = "left", size = 3) +
+  geom_path(data = segments, aes(x = x, y = y), col = "darkgray") + 
+  # Key
+  geom_rect(xmin = 1933, ymin = 70.75, xmax = 1934.3, ymax = 73.25, 
+    fill = "#81c450", color = "white") + 
+  guides(fill = FALSE) + 
+  theme(panel.background = element_rect(fill = "white"),
+    panel.grid.major.y = element_line(color = "gray", linetype = "dotted"),
+    plot.title = element_text(size = rel(2)),
+    axis.ticks.length = unit(0, "cm")) +
+labs(caption = "Source: Flowing Data      *Data between 1916 and 1972 were unavailable") 
+
+return(p)
+}
